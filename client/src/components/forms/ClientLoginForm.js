a3d56809654e9d96ccbba3c261a64931ba8575d0
therealsweven <robssimpson11@gmail.com>
2023-07-25 23:08:20 -0600

    login modal size fixed

--- client/src/components/forms/ClientLoginForm.js
+++ client/src/components/forms/ClientLoginForm.js
@@ -49,7 +49,7 @@ export default function ClientLoginForm() {
       {showModal ? (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl min-w-[40%]">
               {/*content*/}
               <div className="border border-accent p-6 rounded-lg shadow-lg relative flex flex-col w-full bg-secondary outline-none focus:outline-none">
                 {/*header*/}
